b6cd3bf185540839d42730d96ed6ae2608758ec6
filipe <filipelopes15@gmail.com>
2016-09-25 17:20:15 -0300

    Atualiza lista de tokens

--- src/main/java/parser/GrammarSymbols.java
+++ src/main/java/parser/GrammarSymbols.java
@@ -12,7 +12,63 @@ public class GrammarSymbols {
 
 	// Language terminals (starts from 0)
 	public static final int ID = 0;
-	//TODO
+	
+	public static final int IDENTIFICATOR_DIVISION = 1;
+	
+	public static final int DATA_DIVISION = 2;
+	
+	public static final int PROCEDURE_DIVISION = 3;
+	
+	public static final int DOT = 4;
+	
+	public static final int PROGRAMID = 5;
+	
+	public static final int INTEGER = 6;
+	
+	public static final int BOOLEAN = 7;
+	
+	public static final int EXIT = 8;
+	
+	public static final int TO = 9;
+	
+	public static final int MOVE = 10;
+	
+	public static final int PERFORM = 11;
+	
+	public static final int USING = 12;
+			
+	public static final int UNTIL = 13;
+	
+	public static final int PIC = 14;
+	
+	public static final int COMP = 15;
+	
+	public static final int SECTION = 16;
+	
+	public static final int RETURN = 17;
+	
+	public static final int DISPLAY = 18;
+	
+	public static final int PLUS = 19;
+	
+	public static final int MINUS = 20;
+	
+	public static final int MULTIPLICATION = 21;
+	
+	public static final int DIVISION = 22;
+	
+	public static final int IF = 23;
+	
+	public static final int THEN = 24;
+	
+	public static final int ELSE = 25;
+	
+	public static final int END_IF = 26;
+	
+	public static final int END_WHILE = 27;
+	
+	public static final int END_PROC = 28;
+	
 	public static final int EOT = 1000;
 	
 }
