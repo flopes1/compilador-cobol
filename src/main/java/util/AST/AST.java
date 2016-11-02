59efec8daf5f11aa3dfd3f5fcf2c2ee77bbb22b9
filipe <filipelopes15@gmail.com>
2016-11-02 01:51:47 -0300

    Adiciona metodo abstrato visit na classe ast

--- src/main/java/util/AST/AST.java
+++ src/main/java/util/AST/AST.java
@@ -1,23 +1,31 @@
 package util.AST;
 
+import checker.IVisitor;
+
 /**
  * AST class
+ * 
  * @version 2010-september-04
  * @discipline Compiladores
  * @author Gustavo H P Carvalho
  * @email gustavohpcarvalho@ecomp.poli.br
  */
-public abstract class AST {
+public abstract class AST
+{
 
-	public String getSpaces(int level) {
+	public String getSpaces(int level)
+	{
 		StringBuffer str = new StringBuffer();
-		while( level>0 ) {
+		while (level > 0)
+		{
 			str.append(" ");
 			level--;
 		}
 		return str.toString();
 	}
-	
+
 	public abstract String toString(int level);
-	
+
+	public abstract Object visit(IVisitor visitor, Object object);
+
 }
