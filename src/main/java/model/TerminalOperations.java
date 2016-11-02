d4f9eb79ad15c74dc43b6f36afa246ccee5a3443
filipe <filipelopes15@gmail.com>
2016-11-02 01:13:25 -0300

    Remove classes que representavam as operações matematicas individuais e insere uma classe generica que representa as quatro operações para simplificar o checker

---
+++ src/main/java/model/TerminalOperations.java
@@ -0,0 +1,25 @@
+package model;
+
+import scanner.Token;
+
+public class TerminalOperations extends Terminal
+{
+
+	public TerminalOperations(Token token)
+	{
+		super.setToken(token);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return super.toString();
+	}
+	
+	@Override
+	public String toString(int level) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
